ef490c74f7290250b579cbdfcf562c449d83b157
kwlodarczyk1991 <57062476+kwlodarczyk1991@users.noreply.github.com>
2019-11-14 12:28:45 +0100

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,25 @@
+
+import random 
+
+def pick_capital(): 
+    capital_list = ['Warsaw','Berlin','Kiev','Moscow','Roma','Paris']    
+    word = random.choice(capital_list)
+    return word 
+
+pass 
+
+def get_hashed(word): 
+    password = ['_ '] * len(word)
+for char in password: 
+    hashed_password += char + '' 
+
+pass 
+
+def uncover(hashed_password, password, letter) 
+
+
+
+        
+
+
+
